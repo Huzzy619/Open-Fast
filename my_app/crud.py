40d5f60899c49440ab4ebde7815d30ed9ccd15d1
Hussein Ibrahim <hussein@pressone.co>
2024-04-01 08:53:16 +0100

    test comment

--- my_app/crud.py
+++ my_app/crud.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session
 
 from . import models, schemas
 
-
+# This is a test comment
 
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
